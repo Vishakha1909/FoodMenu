46fd878bb322d26b25abeaab199cf1b1041fa162
Vishakha Kumaresan <vishakhakumaresan2001@gmail.com>
2021-11-19 10:34:32 +0530

    updated

--- food_menu.php
+++ food_menu.php
@@ -1,3 +1,12 @@
 <?php
-
+function get_rows() {
+    $file=fopen("food.txt",'r',FILE_IGNORE_NEW_LINES);
+    while($line = fgets($file)){//while we can still read the file
+        $line=trim($line);//remove the line endings and extra white-spaces at start and end
+        list($day,$starter,$main,$dessert) = explode('|',$line);//you get an array of 4 per line, first item is author, etc...
+        echo "<tr><td>$day</td><td>$starter\n---\n$main\n---\n$dessert</td></tr>\n";//use the \n to clean up the source a code a bit to make it readable
+    }
+    return true;
+}
+get_rows();
 ?>
